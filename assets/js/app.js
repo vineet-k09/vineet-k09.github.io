49a17a920075b98526197f9db8a4aaf58c111799
Vineet <vineetkushwaha6325@gmail.com>
2023-05-16 18:58:35 +0530

    bg change and footer betterment

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,7 @@
 
 (function() {
   "use strict";
-
-  /**
-   * Easy selector helper function
-   */
+/* Easy selector helper function*/ 
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -13,10 +10,7 @@
       return document.querySelector(el)
     }
   }
-
-  /**
-   * Easy event listener function
-   */
+/* Easy event listener function*/ 
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
     if (selectEl) {
@@ -27,17 +21,11 @@
       }
     }
   }
-
-  /**
-   * Easy on scroll event listener 
-   */
+/* Easy on scroll event listener */ 
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
-
-  /**
-   * Navbar links active state on scroll
-   */
+/* Navbar links active state on scroll*/ 
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
@@ -54,10 +42,7 @@
   }
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
-
-  /**
-   * Scrolls to an element with header offset
-   */
+/* Scrolls to an element with header offset*/ 
   const scrollto = (el) => {
     let elementPos = select(el).offsetTop
     window.scrollTo({
@@ -65,10 +50,7 @@
       behavior: 'smooth'
     })
   }
-
-  /**
-   * Back to top button
-   */
+/* Back to top button*/ 
   let backtotop = select('.back-to-top')
   if (backtotop) {
     const toggleBacktotop = () => {
@@ -81,19 +63,13 @@
     window.addEventListener('load', toggleBacktotop)
     onscroll(document, toggleBacktotop)
   }
-
-  /**
-   * Mobile nav toggle
-   */
+/* Mobile nav toggle*/ 
   on('click', '.mobile-nav-toggle', function(e) {
     select('body').classList.toggle('mobile-nav-active')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
-
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
+/* Scrool with ofset on links with a class name .scrollto*/ 
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
       e.preventDefault()
@@ -108,48 +84,37 @@
       scrollto(this.hash)
     }
   }, true)
-
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
-  window.addEventListener('load', () => {
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
+/* Scroll with ofset on page load with hash links in the url*/ 
+window.addEventListener('load', () => {
+  if (window.location.hash) {
+    if (select(window.location.hash)) {
         scrollto(window.location.hash)
-      }
     }
-  });
-
-  /**
-   * Preloader
-   */
+  }
+});
+/* Preloader*/ 
   let preloader = select('#preloader');
   if (preloader) {
     window.addEventListener('load', () => {
       preloader.remove()
     });
   }
-
-  /**
-   * Hero type effect
-   */
-  const typed = select('.typed')
-  if (typed) {
-    let typed_strings = typed.getAttribute('data-typed-items')
-    typed_strings = typed_strings.split(',')
-    new Typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
-    });
-  }
-
-  /**
-   * Skills animation
-   */
-  let skilsContent = select('.skills-content');
+/* Hero type effect*/ 
+document.addEventListener('DOMContentLoaded', function(){
+  var typed = new Typed('.typed', {
+  strings: ['Kushwaha'],
+  typeSpeed: 100,
+  loop: true,
+  backDelay: 2000,
+  shuffle: true,
+  cursorChar: '.',
+  // &#9825;
+  showCursor: true,
+  contentType: 'html',
+  });
+});
+/*Skills animation*/
+let skilsContent = select('.skills-content');
   if (skilsContent) {
     new Waypoint({
       element: skilsContent,
@@ -158,60 +123,47 @@
         let progress = select('.progress .progress-bar', true);
         progress.forEach((el) => {
           el.style.width = el.getAttribute('aria-valuenow') + '%'
-        });
-      }
-    })
-  }
-
-  /**
-   * Porfolio isotope and filter
-   */
-  window.addEventListener('load', () => {
+      });
+    }
+  })
+}
+/* Porfolio isotope and filter*/
+window.addEventListener('load', () => {
     let portfolioContainer = select('.portfolio-container');
     if (portfolioContainer) {
       let portfolioIsotope = new Isotope(portfolioContainer, {
         itemSelector: '.portfolio-item'
-      });
+    });
 
-      let portfolioFilters = select('#portfolio-flters li', true);
+    let portfolioFilters = select('#portfolio-flters li', true);
 
-      on('click', '#portfolio-flters li', function(e) {
+    on('click', '#portfolio-flters li', function(e) {
         e.preventDefault();
         portfolioFilters.forEach(function(el) {
           el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
-
-        portfolioIsotope.arrange({
+    });
+    this.classList.add('filter-active');
+    
+    portfolioIsotope.arrange({
           filter: this.getAttribute('data-filter')
-        });
-        portfolioIsotope.on('arrangeComplete', function() {
+    });
+    portfolioIsotope.on('arrangeComplete', function() {
           AOS.refresh()
-        });
-      }, true);
-    }
-
-  });
-
-  /**
-   * Initiate portfolio lightbox 
-   */
+    });
+    }, true);
+  }
+});
+/* Initiate portfolio lightbox*/
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
   });
-
-  /**
-   * Initiate portfolio details lightbox 
-   */
+/* Initiate portfolio details lightbox */ 
   const portfolioDetailsLightbox = GLightbox({
     selector: '.portfolio-details-lightbox',
     width: '90%',
     height: '90vh'
   });
-
-  /**
-   * Portfolio details slider
-   */
+/* Portfolio details slider*/ 
   new Swiper('.portfolio-details-slider', {
     speed: 400,
     loop: true,
@@ -225,10 +177,7 @@
       clickable: true
     }
   });
-
-  /**
-   * Testimonials slider
-   */
+/* Testimonials slider*/ 
   new Swiper('.testimonials-slider', {
     speed: 600,
     loop: true,
@@ -243,10 +192,7 @@
       clickable: true
     }
   });
-
-  /**
-   * Animation on scroll
-   */
+/* Animation on scroll*/ 
   window.addEventListener('load', () => {
     AOS.init({
       duration: 1000,
@@ -256,8 +202,7 @@
     })
   });
 /**
-   * Initiate Pure Counter 
-   */
+   * Initiate Pure Counter */ 
 new PureCounter();
 
 })()
