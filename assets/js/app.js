697cf5d4adc5cdaf94e8da1a7b44e7fe8c01b830
Vineet <vineetkushwaha6325@gmail.com>
2023-05-17 18:06:38 +0530

    indent fix, work / portfolio added added copyright to button

--- assets/js/app.js
+++ assets/js/app.js
@@ -135,20 +135,20 @@ window.addEventListener('load', () => {
 
     let portfolioFilters = select('#portfolio-flters li', true);
 
-    on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-    });
-    this.classList.add('filter-active');
+    // on('click', '#portfolio-flters li', function(e) {
+        // e.preventDefault();
+    // portfolioFilters.forEach(function(el) {
+    //       el.classList.remove('filter-active');
+    // });
+    // this.classList.add('filter-active');
     
-    portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-    });
-    portfolioIsotope.on('arrangeComplete', function() {
-          AOS.refresh()
-    });
-    }, true);
+    // portfolioIsotope.arrange({
+    //       filter: this.getAttribute('data-filter')
+    // });
+    // portfolioIsotope.on('arrangeComplete', function() {
+          // AOS.refresh()
+    // });
+    // }, true);
   }
 });
 /* Initiate portfolio lightbox*/
