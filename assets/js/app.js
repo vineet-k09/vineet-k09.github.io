8d76b285f0442f335edf9861d49f681e6105e983
Vineet <vineetkushwaha6325@gmail.com>
2023-05-22 19:58:40 +0530

    fixed scoll to

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,33 @@
-(function() {
-  "use strict";
+(function() { 
+"use strict";
 /* Easy selector helper function*/ 
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
+const select = (el, all = false) => {
+  el = el.trim()
+  if (all) {
+    return [...document.querySelectorAll(el)]
+  } else {
+    return document.querySelector(el)
   }
+}
 /* Easy event listener function*/ 
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
-      }
+const on = (type, el, listener, all = false) => {
+  let selectEl = select(el, all)
+  if (selectEl) {
+    if (all) {
+      selectEl.forEach(e => e.addEventListener(type, listener))
+    } else {
+      selectEl.addEventListener(type, listener)
     }
   }
+}
 /* Easy on scroll event listener */ 
-  const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
-/* Navbar links active state on scroll*/ 
+const onscroll = (el, listener) => {
+  el.addEventListener('scroll', listener)
+}
+
+  /**
+   * Navbar links active state on scroll
+   */
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
@@ -41,34 +44,29 @@
   }
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
-/* Scrolls to an element with header offset*/ 
-  const scrollto = (el) => {
-    let elementPos = select(el).offsetTop
-    window.scrollTo({
-      top: elementPos,
-      behavior: 'smooth'
-    })
-  }
+
 /* Back to top button*/ 
-  let backtotop = select('.back-to-top')
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active')
-      } else {
-        backtotop.classList.remove('active')
-      }
+let backtotop = select('.back-to-top')
+if (backtotop) {
+  const toggleBacktotop = () => {
+    if (window.scrollY > 100) {
+      backtotop.classList.add('active')
+    } else {
+      backtotop.classList.remove('active')
     }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
   }
+  window.addEventListener('load', toggleBacktotop)
+  onscroll(document, toggleBacktotop)
+}
+
 /* Preloader*/ 
-  let preloader = select('#preloader');
+let preloader = select('#preloader');
   if (preloader) {
     window.addEventListener('load', () => {
       preloader.remove()
-    });
-  }
+  });
+}
+
 /*type animation*/ 
 document.addEventListener('DOMContentLoaded', function(){
   var typed = new Typed('.typed', {
@@ -91,6 +89,7 @@ AOS.init({
   delay: 50, 
   once: true
 });
+
 // custom cursor
 var cursor = document.querySelector('.cursor');
 document.addEventListener('mousemove', function(e) {
@@ -119,4 +118,5 @@ linkSpread.forEach(linkSpread => {
     curContain.classList.remove("large");
   })
 })
+// end
 })()
