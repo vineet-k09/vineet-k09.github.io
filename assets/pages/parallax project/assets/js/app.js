3629191482982d23189689189940142d3a08b5b5
Vineet <vineetkushwaha6325@gmail.com>
2023-09-15 22:55:21 +0530

    updated parallax

--- assets/pages/parallax project/assets/js/app.js
+++ assets/pages/parallax project/assets/js/app.js
@@ -1,15 +1,8 @@
-let mainText = document.getElementById("Mtext");
-let jSparrowImg = document.getElementById("jSparrow");
+let mainText = document.getElementById("mainText");
+let introText = document.getElementsByClassName("introText")
 
 window.addEventListener("scroll", f => {
   let value = window.scrollY;
-  mainText.style.marginRight = value * 4 + "px";
-  mainText.style.marginTop = value * 1  + "px";     
-})
-
-window.addEventListener("scroll", e => {
-    let value2 = window.scrollY;
-    jSparrowImg.style.marginTop = value2 + "px";
-    let scaleValue = (value2 * 0.5) / 100;
-    jSparrowImg.style.transform = "scale(" + scaleValue + ")";
+  mainText.style.right = value * 4 + "px";
+  mainText.style.marginTop = value * 1 + "px"; 
 })
